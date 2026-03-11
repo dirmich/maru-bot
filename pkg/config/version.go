5e545332ae7fab4ec9a4cd465678324ad853bc4f
dirmich <dirmich@gmail.com>
2026-03-11 14:37:58 +0900

    release: v0.4.32 with RAG and reload hotfix

--- pkg/config/version.go
+++ pkg/config/version.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-const Version = "0.4.31"
+const Version = "0.4.32"
 
 // CheckLatestVersion fetches the latest version from the remote repository
 func CheckLatestVersion() (string, error) {
