2960cd451d3a612f2745f877f9e8673a6d2fddab
dirmich <dirmich@gmail.com>
2026-03-20 18:47:45 +0900

    a

--- pkg/config/version.go
+++ pkg/config/version.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-const Version = "0.4.83"
+const Version = "0.4.85"
 
 // CheckLatestVersion fetches the latest version from the remote repository
 func CheckLatestVersion() (string, error) {
